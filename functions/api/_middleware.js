0b2a51e17ab9d6cdc0a71d505a5b121962e30c60
qwe-zwh <qwe20070912@gmail.com>
2026-08-24 18:46:03 +0800

    Strip API CORS response headers

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -7,9 +7,19 @@ const SECURITY_HEADERS = {
   "x-frame-options": "DENY",
 };
 
+const CORS_HEADERS = [
+  "access-control-allow-credentials",
+  "access-control-allow-headers",
+  "access-control-allow-methods",
+  "access-control-allow-origin",
+  "access-control-expose-headers",
+  "access-control-max-age",
+];
+
 export async function onRequest({ next }) {
   const response = await next();
   const headers = new Headers(response.headers);
+  CORS_HEADERS.forEach(name => headers.delete(name));
   Object.entries(SECURITY_HEADERS).forEach(([name, value]) => headers.set(name, value));
   return new Response(response.body, {
     status: response.status,
